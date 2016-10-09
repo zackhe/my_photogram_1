c86fa516b6cc2f7bd9e3d5c5eafa5460bd8032be
Murugan <murugu2012@gmail.com>
2016-10-09 11:30:36 +0530

    Direct association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :photos,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
